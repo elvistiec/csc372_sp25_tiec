dd10f6ef0f7494b047f63b782cd80ad36b9e0dba
elvis tiec <73971286+elvistiec@users.noreply.github.com>
2025-02-10 14:55:11 -0500

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,16 @@
+# 6 arrays to represent 6 faces
+# white, green, blue, orange, red, yellow
+cube = [
+    ['w', 'w', 'w', 'w'],
+    ['g', 'g', 'g', 'g'],
+    ['b', 'b', 'b', 'b'],
+    ['o', 'o', 'o', 'o'],
+    ['r', 'r', 'r', 'r'],
+    ['y', 'y', 'y', 'y'],
+]
+
+#reset cube to solved state
+def reset_cube():
+
+#boolean to check if the cube is solved
+def is_solved(cube):
