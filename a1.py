7657d0e669b63d732ee5439a7a9d1344b351ee62
elvis tiec <73971286+elvistiec@users.noreply.github.com>
2025-02-12 08:18:36 -0500

    Update a1.py

--- a1.py
+++ a1.py
@@ -10,7 +10,20 @@ cube = [
 ]
 
 #reset cube to solved state
-def reset_cube():
+def reset_cube(cube):
 
 #boolean to check if the cube is solved
 def is_solved(cube):
+
+#set a certain face on the front
+def front(int i):
+
+#rotate clockwise
+def rotate_cw(int i):
+
+#rotate counter clockwise
+def rotate_ccw(int i):
+
+#randomize the cube inputting how many desired rotations
+def randomize(cube):
+
