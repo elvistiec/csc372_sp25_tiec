1e31d10545afb6c6af4b9a53fc69835ffecc80fd
elvis tiec <73971286+elvistiec@users.noreply.github.com>
2025-02-12 18:37:09 -0500

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,29 +1,33 @@
-# 6 arrays to represent 6 faces
+# 6 arrays to represent 6 faces (object)
 # white, green, blue, orange, red, yellow
+# numbers represent the location of the sticker on the cube
+# [1,2]
+# [3,4]
 cube = [
-    ['w', 'w', 'w', 'w'],
-    ['g', 'g', 'g', 'g'],
-    ['b', 'b', 'b', 'b'],
-    ['o', 'o', 'o', 'o'],
-    ['r', 'r', 'r', 'r'],
-    ['y', 'y', 'y', 'y'],
+    ['w1', 'w2', 'w3', 'w4'],
+    ['g1', 'g2', 'g3', 'g4'],
+    ['b1', 'b2', 'b3', 'b4'],
+    ['o1', 'o2', 'o3', 'o4'],
+    ['r1', 'r2', 'r3', 'r4'],
+    ['y1', 'y2', 'y3', 'y4'],
 ]
 
-#reset cube to solved state
+#reset cube to solved state (void)
 def reset_cube(cube):
 
-#boolean to check if the cube is solved
+#check if the cube is solved (bool)
 def is_solved(cube):
 
-#set a certain face on the front
+#set a certain face on the front (void)
+#input the face you want on the front
 def front(int i):
 
-#rotate clockwise
-def rotate_cw(int i):
+#rotate front face clockwise (void)
+def rotate_right(int i):
 
-#rotate counter clockwise
-def rotate_ccw(int i):
+#rotate front face counter clockwise (void)
+def rotate_left(int i):
 
-#randomize the cube inputting how many desired rotations
-def randomize(cube):
+#randomize the cube inputting how many desired moves you want (void)
+def randomize(cube, i):
 
